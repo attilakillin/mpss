71486163a19b73f4027498fd6463ac3f4b05227b
Zoltan Karsa <61280910+karsazoltan@users.noreply.github.com>
2022-10-24 15:45:09 +0200

    production getbyid and post

--- src/main/kotlin/hu/swarch/mpss/web/ProductionGoalController.kt
+++ src/main/kotlin/hu/swarch/mpss/web/ProductionGoalController.kt
@@ -1,10 +1,15 @@
 package hu.swarch.mpss.web
 
 import hu.swarch.mpss.dal.ProductionGoalRepository
+import hu.swarch.mpss.entities.ProductionGoal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import java.time.Instant
 
 @Controller
 @RequestMapping("/production_goals")
@@ -19,5 +24,28 @@ class ProductionGoalController(
         model.addAttribute("goals", goals)
 
         return "production_goals"
+    }
+
+    @GetMapping("/{id}")
+    fun getGoalDetail(@PathVariable id: Long, model: Model): String {
+        val goal = productionGoalRepository.findById(id)
+
+        model.addAttribute("goal", goal)
+
+        return "goal_details"
+    }
+
+    @GetMapping("/create")
+    fun getCreateGoal(model: Model): String {
+        model.addAttribute("goal", ProductionGoal(0, arrayListOf(), Instant.now()))
+        model.addAttribute("products_goal", null)
+        return "goal_create"
+    }
+
+    @PostMapping("/create")
+    fun postGoal(@ModelAttribute goal: ProductionGoal, model: Model): String {
+        val goal = productionGoalRepository.save(goal)
+        model.addAttribute("goal", goal)
+        return "goal_create"
     }
 }
