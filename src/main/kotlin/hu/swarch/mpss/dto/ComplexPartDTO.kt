53bc8b4177f38c6db4b1679283571cbb7256c92a
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-11-19 22:43:38 +0100

    Implement complete Complex Part endpoint - New Service and DTO classes - Validation inside service class - New, Edit, and Delete functions tested on frontend

---
+++ src/main/kotlin/hu/swarch/mpss/dto/ComplexPartDTO.kt
@@ -0,0 +1,8 @@
+package hu.swarch.mpss.dto
+
+data class ComplexPartDTO(
+    val id: Long?,
+    val name: String,
+    val subparts: Map<Long, Int>,
+    val constructionTime: String
+)
---
+++ src/main/kotlin/hu/swarch/mpss/services/ComplexPartService.kt
@@ -0,0 +1,56 @@
+package hu.swarch.mpss.services
+
+import hu.swarch.mpss.dal.PartRepository
+import hu.swarch.mpss.dto.ComplexPartDTO
+import hu.swarch.mpss.dto.toDuration
+import hu.swarch.mpss.entities.ComplexPart
+import hu.swarch.mpss.entities.Part
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class ComplexPartService(
+    private val partRepository: PartRepository
+) {
+    private fun dtoToEntity(dto: ComplexPartDTO): ComplexPart? {
+        if (dto.name.isBlank() || dto.subparts.isEmpty()) return null
+        val constructionTime = dto.constructionTime.toDuration() ?: return null
+
+        val subparts: MutableMap<Part, Int> = mutableMapOf()
+        for ((subpartId, count) in dto.subparts) {
+            if (count == 0 || dto.id == subpartId) return null
+            val subpart = partRepository.findByIdOrNull(subpartId) ?: return null
+            subparts[subpart] = count
+        }
+
+        return ComplexPart(
+            name = dto.name,
+            constructionTime = constructionTime,
+            subparts = subparts
+        )
+    }
+
+    fun findAllComplexParts(): List<ComplexPart> {
+        return partRepository.findAllComplexParts()
+    }
+
+    fun saveComplexPart(dto: ComplexPartDTO): ComplexPart? {
+        if (dto.id != null) return null
+
+        val part = dtoToEntity(dto) ?: return null
+        partRepository.save(part)
+        return part
+    }
+
+    fun updateComplexPart(dto: ComplexPartDTO): ComplexPart? {
+        if (dto.id == null || !partRepository.existsById(dto.id)) return null
+
+        val part = dtoToEntity(dto)?.also { it.id = dto.id } ?: return null
+        partRepository.save(part)
+        return part
+    }
+
+    fun deleteComplexPart(id: Long) {
+        partRepository.deleteById(id)
+    }
+}
